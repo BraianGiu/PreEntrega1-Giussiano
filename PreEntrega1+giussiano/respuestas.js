829ee3a747389b217f7ff03845c8498bf9805737
Braian G <129406743+BraianGiu@users.noreply.github.com>
2023-05-30 16:12:18 -0300

    Add files via upload

---
+++ PreEntrega1+giussiano/respuestas.js
@@ -0,0 +1,35 @@
+function resultado() {
+    var p1, p2, p3, p4, p5, p6, p7, p8, p9, nota;
+
+    // 1
+    if (document.getElementById('p11').checked==true) {p1=1}
+    else {p1=0}
+    // 2
+    if (document.getElementById('p24').checked==true) {p2=1}
+    else {p2=0}
+    // 3
+    if (document.getElementById('p41').checked==true) {p3=1}
+    else {p3=0}
+    // 4
+    if (document.getElementById('p52').checked==true) {p4=1}
+    else {p4=0}
+    // 5
+    if (document.getElementById('p61').checked==true) {p5=1}
+    else {p5=0}
+    // 6
+    if (document.getElementById('p73').checked==true) {p6=1}
+    else {p6=0}
+    // 7
+    if (document.getElementById('p88').checked==true) {p7=1}
+    else {p7=0}
+    // 8
+    if (document.getElementById('p91').checked==true) {p8=1}
+    else {p8=0}
+    // 9
+    if (document.getElementById('p01').checked==true) {p9=1}
+    else {p9=0}
+
+    nota=p1+p2+p3+p4+p5+p6+p7+p8+p9;
+    alert(" Aciertos: " + nota);
+    window.location = 'cuestionario.html'
+}
